00ef927303aa5fd54a0b9e0fa402775620b3c710
James Batsell <jamesbatsell05@gmail.com>
2020-04-09 23:40:10 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,7 +1,7 @@
-class CreateGenres < ActiveRecord::Migration[5.2]
-  
-    def change create_table :genres do |j|
-    j.string :name
+class CreateGenres < ActiveRecord::Migration[4.2]
+  def change
+    create_table :genres do |j|
+      j.string :name
     end
   end
 end
